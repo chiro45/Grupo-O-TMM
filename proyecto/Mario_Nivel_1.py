327743585cb03a19e89fe50222f93430a0c7d598
chiro45 <luciano.chiroli45@gmail.com>
2022-06-17 21:48:57 -0300

    arreglar importacion

--- proyecto/Mario_Nivel_1.py
+++ proyecto/Mario_Nivel_1.py
@@ -4,11 +4,11 @@ Este es un intento de recrear el primer nivel del Super Mario Bros
 
 import sys
 import pygame as pg
-from data.main import main
+from Data.main import main
 import cProfile
 
 
-if __nombre__=='__main__':
+if __nombre__ =='__main__':
     main()
     pg.quit()
     sys.exit()
